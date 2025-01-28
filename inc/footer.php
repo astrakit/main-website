3c0280be1640886167fd29b977f067a0e2fbc961
catpawz <cat@catpawz.net>
2025-01-28 20:11:14 +0100

    Add privacy policy page and update footer links for improved navigation

--- inc/footer.php
+++ inc/footer.php
@@ -17,6 +17,7 @@
                         <li><a href="#" class="hover:text-[#43ffe3]">Official channel</a></li>
                         <li><a href="#" class="hover:text-[#43ffe3]">Roadmap</a></li>
                         <li><a href="#" class="hover:text-[#43ffe3]">Partners</a></li>
+                        <li><a href="https://cat-space.net/@astrakit" class="hover:text-[#43ffe3]">Mastodon / Misskey</a></li>
                     </ul>
                 </div>
                 <div>
@@ -24,24 +25,24 @@
                     <ul class="text-lg text-gray-300 space-y-2">
                         <li><a href="#" class="hover:text-[#43ffe3]">About Us</a></li>
                         <li><a href="#" class="hover:text-[#43ffe3]">Careers</a></li>
-                        <li><a href="#" class="hover:text-[#43ffe3]">Contact</a></li>
+                        <li><a href="/contact" class="hover:text-[#43ffe3]">Contact</a></li>
                         <li><a href="https://github.com/astrakit" class="hover:text-[#43ffe3]">GitHub</a></li>
                     </ul>
                 </div>
                 <div>
                     <h3 class="text-lg font-semibold text-gray-100 mb-4">Legal & Security</h3>
                     <ul class="text-lg text-gray-300 space-y-2">
-                        <li><a href="#" class="hover:text-[#43ffe3]">Privacy policy</a></li>
-                        <li><a href="#" class="hover:text-[#43ffe3]">Terms of service</a></li>
-                        <li><a href="#" class="hover:text-[#43ffe3]">Data transparency</a></li>
-                        <li><a href="#" class="hover:text-[#43ffe3]">Report security issue</a></li>
+                        <li><a href="/privacy" class="hover:text-[#43ffe3]">Privacy policy</a></li>
+                        <li><a href="/tos" class="hover:text-[#43ffe3]">Terms of service</a></li>
+                        <li><a href="/data-transparency" class="hover:text-[#43ffe3]">Data transparency</a></li>
+                        <li><a href="/contact" class="hover:text-[#43ffe3]">Report security issue</a></li>
                     </ul>
                 </div>
             </div>
         </div>
         <div class="max-w-3xl mx-auto pt-10 text-center">
             <img src="logo-long.png" alt="Astrakit Logo" class="mx-auto mb-4" style="height: 100px;">
-            <span class="text-lg text-[#43ffe3] block">©<a href="https://astrokit.cc/">Astrakit</a> 2024 - Present, All rights reserved.</span>
+            <span class="text-lg text-[#43ffe3] block">©<a href="https://astrokit.cc/">Astrakit</a> 2024 - <?php echo date("Y"); ?>, All rights reserved.</span>
         </div>
     </div>
 </footer>
